efa08bb30a649af0b4fe9f321a7a50cf5739bf80
hejun <daniel_jhe@vipabc.com>
2018-08-29 19:42:37 +0800

    a

--- src/commands/Action.js
+++ src/commands/Action.js
@@ -1,5 +1,5 @@
 /**
- * Action of redo-undo.
+ * enum of action type
  */
 const ActionType = {
   "translate": "translate",
@@ -9,74 +9,80 @@ const ActionType = {
   "typing": "typing",
 }
 
-
 /**
- * Get anti-action for current action
- * @param {*} delta
- * @param {*} oldDelta used fro anti-action
+ * Action of redo-undo history
  */
-function getUndoAction(delta, oldDelta) {
-  if (delta.action === 'ADD') {
-    return Object.assign({}, delta, {
-      action: 'DELETE',
-    });
-  } else if (delta.action === 'DELETE') {
-    return Object.assign({}, delta, {
-      action: 'ADD',
-    });
-  } else if (delta.action === 'MOVE') {
-    let antiOffset = {
-      x: -delta.data.offset.x,
-      y: -delta.data.offset.y,
-    };
-
-    return {
-      action: 'MOVE',
-      data: {
-        ids: delta.data.ids,
-        offset: antiOffset,
-      },
-    };
-  } else if (delta.action === 'SCALE') {
-    let antiScale = {
-      sx: 1 / delta.data.scale.sx,
-      sy: 1 / delta.data.scale.sy,
-      basePoint: delta.data.scale.basePoint,
-    };
-    return {
-      action: 'SCALE',
-      data: {
-        ids: delta.data.ids,
-        scale: antiScale,
-      },
-    };
-  } else if (delta.action === 'TYPING') {
-    return {
-      action: 'TYPING',
-      data: {
-        id: delta.data.id,
-        value: delta.data.lastTypedText,
-      },
-    };
-  }
-}
 export default class Action {
 
   recordTimeStamp = +new Date();
+  _redo = null;
+  _undo = null;
+  _hot = 0; // 操作热度计数
 
-  constructor(type) {
-    this.type = type;
+  constructor(delta, oldDelta = null) {
+    this.type = delta.type;
+    this._redo = {
+
+    }
   }
 
-  get redo(){
-
+  /**
+   * Get undo-action for current action
+   * @param {Object} delta
+   * @param {Object} oldDelta used fro anti-action
+   */
+  _calcUndo(delta, oldDelta) {
+    let hash = delta.data;
+    switch (delta.action) {
+      case 'ADD':
+        return Object.assign({}, delta, {
+          action: 'DELETE'
+        });
+      case 'DELETE':
+        return Object.assign({}, delta, {
+          action: 'ADD'
+        });
+      case 'MOVE':
+        let { x, y } = hash.offset;
+        return {
+          action: 'MOVE',
+          data: {
+            ids: hash.ids,
+            offset: { x: -x, y: -y },
+          },
+        };
+      case 'SCALE':
+        let {
+          sx,
+          sy,
+          basePoint
+        } = hash.scale;
+        return {
+          action: 'SCALE',
+          data: {
+            ids: hash.ids,
+            scale: {
+              sx: 1 / sx,
+              sy: 1 / sy,
+              basePoint
+            },
+          },
+        };
+      case 'TYPING':
+        return {
+          action: 'TYPING',
+          data: {
+            id: hash.id,
+            value: hash.lastTypedText,
+          },
+        };
+    }
   }
-
-  get undo(){
-
+  
+  get redo() {
+    return this._redo;
   }
-
-  get data() {
-
+  get undo() {
+    return this._undo;
   }
 }
